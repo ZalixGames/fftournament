4b0a6456575061a1f2379a3e06368e97bec76c85
ZalixGames <7sstock009@gmail.com>
2025-07-04 04:38:48 -0700

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,82 @@
+const CACHE_NAME = 'gaming-tournament-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'images/icons/icon-192x192.png',
+  'images/icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+  // Note: Firebase SDKs are loaded via type="module" and might not be cacheable this way directly.
+  // The browser will handle them, but the core app shell will be offline.
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    // We only want to cache GET requests.
+    if (event.request.method !== 'GET') {
+        return;
+    }
+    
+    // For other requests, go to the network first, then cache.
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                // Check if we received a valid response
+                if (!response || response.status !== 200 || response.type !== 'basic' && response.type !== 'cors') {
+                    // if not, return the response as is
+                    return response;
+                }
+                
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                const responseToCache = response.clone();
+                
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, responseToCache);
+                    });
+                
+                return response;
+            })
+            .catch(() => {
+                // If the network fails, try to get it from the cache.
+                return caches.match(event.request);
+            })
+    );
+});
+
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
